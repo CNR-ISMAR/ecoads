1fe654653b569f3c24435bc6a0b359092d0ba916
ElisabethDemaio <elisabeth.demaio@gmail.com>
2020-04-23 12:16:31 +0200

    modified ecos models

--- ecos/models.py
+++ ecos/models.py
@@ -3,42 +3,46 @@
 from django.db import models
 from django.contrib.gis.db import models
 
-class Directive(models.Model):
-    directive_name = models.CharField(max_length=200)
-
-    def _str_(self): 
-        return self.directive_name
-
-class Parameter(models.Model):
-    parameter_name = models.CharField(max_length=200)
-    #choices  array 
-    #@todo lista in attesa del confronto con gli altri 
-
-    def _str_(self): 
-        return self.parameter_name
-
 class DataSource(models.Model):
-    datasource_name= models.CharField(max_length=200, blank=True, null=False)
+    datasource_name = models.CharField(max_length=200, blank=True, null=False)
+    observation_model = models.CharField(max_length=200, blank=True, null=False)
+    location = models.GeometryField(blank=False, null=True )
+    #variables = models.TextField(blank=False, null=True )
+    domain_area = models.MultiPolygonField(blank=False, null=True )
+    update_frequency = models.CharField(max_length=200, blank=True, null=False)
+    sampling_frequency = models.CharField(max_length=200, blank=True, null=False)
+    temporal_resolution = models.CharField(max_length=200, blank=True, null=False)
+    temporal_coverage = models.CharField(max_length=200, blank=True, null=False)
 
     def _str_(self): 
         return self.datasource_name
 
-class EcoadsSite(models.Model):
-    denomination = models.CharField(max_length=200)
-    description = models.TextField()
-    domain_area = models.MultiPolygonField()  #il poligono
-    location = models.GeometryField() #il punto che lo identifica
-    website = models.CharField(max_length=600) #link
-    parameters = models.ManyToManyField(Parameter, through='EcoadsSitesParameters')  
-    data_source = models.ForeignKey(DataSource, on_delete=models.CASCADE)
+class Parameter(models.Model):
+    uri = models.URLField(max_length=600,blank=False, null=True )  
+    preferred_label_en = models.CharField(max_length=500, blank=False, null=True)
+    definition_en = models.TextField(blank=False, null=True)
+    data_source = models.ManyToManyField(DataSource, through='ParametersDataSources')
 
+    def _str_(self): 
+        return self.preferred_label_en
+
+class EcosSite(models.Model):
+    denomination = models.CharField(max_length=200)
+    description = models.TextField(blank=False, null=True )
+    domain_area = models.MultiPolygonField(blank=False, null=True )  #il poligono
+    location = models.GeometryField() #il punto che lo identifica
+    website = models.URLField(max_length=600,blank=False, null=True )
+    data_source = models.ManyToManyField(DataSource, through='EcosSitesDataSources')  
 
     def _str_(self):
         return self.denomination
 
+class EcosSitesDataSources(models.Model): 
+    ecos_site = models.ForeignKey(EcosSite, on_delete=models.CASCADE)
+    data_source = models.ForeignKey(DataSource, on_delete=models.CASCADE)
+    
+class ParametersDataSources(models.Model): 
+    parameter = models.ForeignKey(Parameter, on_delete=models.CASCADE) 
+    data_source = models.ForeignKey(DataSource, on_delete=models.CASCADE)
 
-class EcoadsSitesParameters(models.Model): 
-    parameter = models.ForeignKey(Parameter, on_delete=models.CASCADE)
-    ecoadsSite = models.ForeignKey(EcoadsSite, on_delete=models.CASCADE)
-    #qui aggiungo le colonne che voglio in più
     
