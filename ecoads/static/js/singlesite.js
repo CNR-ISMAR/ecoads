cd5a52d0b290ff4b613f69faf8b2b4c522fb1b77
ElisabethDemaio <elisabeth.demaio@gmail.com>
2021-04-08 16:59:02 +0200

    fixed-point on single site map

--- ecoads/static/js/singlesite.js
+++ ecoads/static/js/singlesite.js
@@ -1,49 +1,70 @@
-    var singlesite = JSON.parse(document.getElementById('singlesite').textContent);
-    var polygon = JSON.parse(document.getElementById('polygon').textContent);
-    
-    var map = L.map('mapsite', {
-    maxZoom: 14,
-    zoom: 10,
-    center:singlesite,
-    fullscreenControl: true,
-    });
+var singlesite = JSON.parse(document.getElementById('singlesite').textContent);
+var polygon = JSON.parse(document.getElementById('polygon').textContent);
 
-    L.tileLayer('http://tile.stamen.com/toner-lite/{z}/{x}/{y}.png', {
-    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+var map = L.map('mapsite', {
+maxZoom: 14,
+zoom: 10,
+center:singlesite,
+fullscreenControl: true,
+});
+
+L.tileLayer('http://tile.stamen.com/toner-lite/{z}/{x}/{y}.png', {
+attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+}).addTo(map);
+
+L.tileLayer('http://tiles.openseamap.org/seamark/{z}/{x}/{y}.png', {
+attribution: ''
+}).addTo(map);
+
+var bathymetryLayer = L.tileLayer.wms("http://ows.emodnet-bathymetry.eu/wms", {
+        layers: 'emodnet:mean_atlas_land',
+        format: 'image/png',
+        transparent: true,
+        attribution: "EMODnet Bathymetry",
+        opacity: 0.5
     }).addTo(map);
 
-    L.tileLayer('http://tiles.openseamap.org/seamark/{z}/{x}/{y}.png', {
-    attribution: ''
-    }).addTo(map);
+var markersite = L.icon({
+    iconUrl: '/media/images/markersite.max-165x165.png',
+    iconSize: [45, 45],
+    popupAnchor: [0, -20],
+});
 
-    var bathymetryLayer = L.tileLayer.wms("http://ows.emodnet-bathymetry.eu/wms", {
-            layers: 'emodnet:mean_atlas_land',
-            format: 'image/png',
-            transparent: true,
-            attribution: "EMODnet Bathymetry",
-            opacity: 0.5
-        }).addTo(map);
+var denomination = JSON.parse(document.getElementById('denomination').textContent); 
 
-    var markersite = L.icon({
-        iconUrl: '/media/images/markersite.max-165x165.png',
-        iconSize: [45, 45],
-        popupAnchor: [0, -20],
-    });
+var marker = new L.marker(singlesite, {icon: markersite})
+        .bindPopup(denomination)
+        .addTo(map);
 
-    var denomination = JSON.parse(document.getElementById('denomination').textContent); 
-    
-    var marker = new L.marker(singlesite, {icon: markersite})
-            .bindPopup(denomination)
-            .addTo(map);
+var stylepolygons = {
+color: "#590416",
+weight: 1,
+opacity: 0.20
+};
 
-    var stylepolygons = {
-    color: "#590416",
-    weight: 1,
-    opacity: 0.20
-    };
-    
-    if (polygon!="") {
-        var layer = L.geoJSON(polygon, {style: stylepolygons}).addTo(map);
-        layer.addData(polygon);
-        map.fitBounds(layer.getBounds(),{maxZoom : 10});
-    };
+if (polygon!="") {
+    var layer = L.geoJSON(polygon, {style: stylepolygons}).addTo(map);
+    layer.addData(polygon);
+    map.fitBounds(layer.getBounds(),{maxZoom : 10});
+};
+
+  // aggiungere json
+
+var icon_fix = L.icon({
+    iconUrl: '/media/images/fix_2.0.original.png',
+    iconSize: [20, 20],
+    iconAnchor: [-1, -1],
+    popupAnchor: [0, -10],
+});
+
+$.getJSON("/djmeasurements/locations/flatjson", function(fixpoints){
+    for (var i = 0; i < fixpoints.length; i++) {
+
+        //console.log(fixpoints[i].name)
+        //console.log(fixpoints[i].latitute)
+        //console.log(fixpoints[i].longitude)
+        markerfix = new L.marker([fixpoints[i].latitute, fixpoints[i].longitude], { icon: icon_fix })
+        .bindPopup(fixpoints[i].name)
+        .addTo(map);
+    }
+});  
