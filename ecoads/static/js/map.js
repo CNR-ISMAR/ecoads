3fda3f9fc701282cca27e63be5604595311b1f20
ElisabethDemaio <elisabeth.demaio@gmail.com>
2021-05-04 12:20:14 +0200

    fixed base layer map

--- ecoads/static/js/map.js
+++ ecoads/static/js/map.js
@@ -1,4 +1,4 @@
-var tonerlite = L.tileLayer('http://tile.stamen.com/toner-lite/{z}/{x}/{y}.png', {
+var tonerlite = L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png', {
     attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
 });
 
