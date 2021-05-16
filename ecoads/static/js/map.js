b9f962053da60e01002db69614f76a5ac3b89c4e
ElisabethDemaio <elisabeth.demaio@gmail.com>
2021-05-16 20:43:09 +0200

    added layer on map 2

--- ecoads/static/js/map.js
+++ ecoads/static/js/map.js
@@ -1,4 +1,4 @@
-var terrain = L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/terrain-background/{z}/{x}/{y}.png', {
+var terrain = L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.png', {
     attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
 });
 
