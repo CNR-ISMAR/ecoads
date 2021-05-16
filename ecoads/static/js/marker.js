d8569f2cb8d8c343812cb6efb27b2794a8531e63
ElisabethDemaio <elisabeth.demaio@gmail.com>
2021-05-16 19:58:18 +0200

    design update - marker other sites

--- ecoads/static/js/marker.js
+++ ecoads/static/js/marker.js
@@ -58,7 +58,7 @@ legend.onAdd = function(map) {
 
     var div = L.DomUtil.create('div', 'info legend'),
         grades = ['ECOAdS sites', 'Other LTER Sites', 'Fixed-Point Observing Systems'];
-    labels = ['/media/images/Certificate_7.original.png', '/media/images/Certificate_7_1.original.pngg', '/media/images/Bullseye_7.original.png'];
+    labels = ['/media/images/Certificate_7.original.png', '/media/images/Certificate_7_1.original.png', '/media/images/Bullseye_7.original.png'];
 
     for (var i = 0; i < grades.length; i++) {
         div.innerHTML +=
